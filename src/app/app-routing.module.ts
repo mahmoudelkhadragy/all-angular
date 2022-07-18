f8925848eb09b726d74dc8dae35e72b8937a0fc4
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2022-07-18 06:09:33 +0200

    start app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { PassGeneratorComponent } from './components/pass-generator/pass-generator.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  { path: 'home', component: HomeComponent },
+  { path: 'pass-generator', component: PassGeneratorComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
