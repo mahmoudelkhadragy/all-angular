63974f3c3b1f4152f89b084709562b227edfb997
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2022-07-18 06:10:04 +0200

    generate pass component

---
+++ src/app/components/pass-generator/pass-generator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pass-generator',
+  templateUrl: './pass-generator.component.html',
+  styleUrls: ['./pass-generator.component.scss']
+})
+export class PassGeneratorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
