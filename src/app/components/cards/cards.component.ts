c53e4c110b3b1c09900ce548bbb4f1a13101c6f6
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2022-08-06 15:18:23 +0200

    cards component

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -3,34 +3,31 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
-  styleUrls: ['./cards.component.scss']
+  styleUrls: ['./cards.component.scss'],
 })
 export class CardsComponent implements OnInit {
-
   posts = [
     {
       title: 'title1',
       imageUrl: 'assets/images/tree.jpeg',
       username: '@nature',
-      content: 'Saw this awesome tree during my hike today.'
+      content: 'Saw this awesome tree during my hike today.',
     },
     {
       title: 'title2',
       username: '@nature',
       imageUrl: 'assets/images/mountain.jpeg',
-      content: 'Saw this awesome tree during my hike today.'
+      content: 'Saw this awesome tree during my hike today.',
     },
     {
       title: 'title3',
       username: '@nature',
       imageUrl: 'assets/images/biking.jpeg',
-      content: 'Saw this awesome tree during my hike today.'
-    }
-  ]
+      content: 'Saw this awesome tree during my hike today.',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
