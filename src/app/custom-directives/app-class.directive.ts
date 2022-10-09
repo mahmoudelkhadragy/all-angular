dbbdf78d4333ec053c750024cccf3587b093fc63
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2022-10-10 00:09:02 +0200

    add appClass istead of ngClass

--- src/app/custom-directives/app-class.directive.ts
+++ src/app/custom-directives/app-class.directive.ts
@@ -5,9 +5,20 @@ import { Directive, ElementRef, Input } from '@angular/core';
 })
 export class AppClassDirective {
   constructor(private element: ElementRef) {}
-
-  @Input() set backgroundColor(color: string){
+  /* se backgroundColor
+  @Input('appClass') set backgroundColor(color: string){
     this.element.nativeElement.style.backgroundColor = color;
+  }
+  */
+
+  @Input('appClass') set classNames(classObj: any){
+    for(let key in classObj){
+      if(classObj[key]){
+        this.element.nativeElement.classList.add(key)
+      }else{
+        this.element.nativeElement.classList.remove(key)
+      }
+    }
   }
 
 }
