79ba855ea250938d4a0839cd064d36b5a752bbd2
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2023-01-15 00:18:07 +0200

    add divider component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RepeatElementDirective } from '../custom-directives/repeat-element.directive';
+import { DividerComponent } from './components/divider/divider.component';
 
 
 
 @NgModule({
-  declarations: [RepeatElementDirective],
+  declarations: [RepeatElementDirective, DividerComponent],
   imports: [
     CommonModule,
   ],
   exports: [
-    RepeatElementDirective
+    RepeatElementDirective,
+    DividerComponent
   ]
 })
 export class SharedModule { }
