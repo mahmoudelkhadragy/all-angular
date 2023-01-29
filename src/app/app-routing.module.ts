d20758b51c9350736ac6ed4c4f3d3769b95e7088
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2023-01-29 20:53:09 +0200

    add mods module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,6 +12,7 @@ const routes: Routes = [
   { path: 'elements', loadChildren: () => import('./elements/elements.module').then((m)=> m.ElementsModule)},
   { path: 'collections', loadChildren: () => import('./collections/collections.module').then((m)=> m.CollectionsModule)},
   { path: 'views', loadChildren: () => import('./views/views.module').then((m)=> m.ViewsModule)},
+  { path: 'mods', loadChildren: () => import('./mods/mods.module').then((m)=> m.ModsModule)},
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'pass-generator', component: PassGeneratorComponent },
