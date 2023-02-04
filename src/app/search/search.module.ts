bea5298d4ead7b2564f0fbbfee6b63e5c88e8cf2
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2023-02-04 16:51:55 +0200

    add shared module

--- src/app/search/search.module.ts
+++ src/app/search/search.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -17,7 +18,8 @@ import { PageListComponent } from './page-list/page-list.component';
   imports: [
     CommonModule,
     SearchRoutingModule,
-    SharedModule
+    SharedModule,
+    HttpClientModule
   ]
 })
 export class SearchModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RepeatElementDirective } from '../custom-directives/repeat-element.directive';
 import { DividerComponent } from './components/divider/divider.component';
+import { TableComponent } from './components/table/table.component';
 
 
 
 @NgModule({
-  declarations: [RepeatElementDirective, DividerComponent],
+  declarations: [RepeatElementDirective, DividerComponent, TableComponent],
   imports: [
     CommonModule,
   ],
   exports: [
     RepeatElementDirective,
-    DividerComponent
+    DividerComponent,
+    TableComponent
   ]
 })
 export class SharedModule { }
