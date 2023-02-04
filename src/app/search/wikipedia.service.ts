15561684a8a92488076538fbe93f0df4471334ff
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2023-02-04 16:51:38 +0200

    add wikipedia service

--- src/app/search/wikipedia.service.ts
+++ src/app/search/wikipedia.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,9 +6,18 @@ import { Injectable } from '@angular/core';
 })
 export class WikipediaService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   search(term: string){
-    return `here is from service ${term}`;
+    return this.http.get('https://en.wikipedia.org/w/api.php', {
+      params: {
+        action: 'query',
+        format: 'json',
+        list: 'search',
+        utf8: '1',
+        srsearch: term,
+        origin: '*'
+      }
+    })
   }
 }
