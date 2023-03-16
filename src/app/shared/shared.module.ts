239248d8f8f36b923c60705f59febab0b7ed85ea
mahmoudelkhadragy <melkhad@emea.corpdir.net>
2023-03-16 15:22:19 +0200

    add input component to sahred

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,18 +3,23 @@ import { CommonModule } from '@angular/common';
 import { RepeatElementDirective } from '../custom-directives/repeat-element.directive';
 import { DividerComponent } from './components/divider/divider.component';
 import { TableComponent } from './components/table/table.component';
+import { InputComponent } from './components/input/input.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
-  declarations: [RepeatElementDirective, DividerComponent, TableComponent],
+  declarations: [RepeatElementDirective, DividerComponent, TableComponent, InputComponent],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   exports: [
     RepeatElementDirective,
     DividerComponent,
-    TableComponent
+    TableComponent,
+    InputComponent
   ]
 })
 export class SharedModule { }
